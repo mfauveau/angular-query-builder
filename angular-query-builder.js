fe47d1e32c795a4b0ab531eda6efb02889602d9b
Matthieu Fauveau <mfauveau@gmail.com>
2014-07-17 22:57:48 -0400

    First commit

---
+++ angular-query-builder.js
@@ -0,0 +1,96 @@
+var app = angular.module('app', ['ngSanitize', 'queryBuilder']);
+app.controller('QueryBuilderCtrl', ['$scope', function ($scope) {
+    var data = '{"group": {"operator": "AND","rules": []}}';
+
+    function computed(group) {
+        if (!group) return "";
+        for (var str = "(", i = 0; i < group.rules.length; i++) {
+            i > 0 && (str += " <strong>" + group.operator + "</strong> ");
+            str += group.rules[i].group ?
+                computed(group.rules[i].group) :
+                group.rules[i].field + " " + group.rules[i].condition + " " + group.rules[i].data;
+        }
+
+        return str + ")";
+    }
+
+    $scope.json = null;
+
+    $scope.filter = JSON.parse(data);
+
+    $scope.$watch('filter', function (newValue) {
+        $scope.json = JSON.stringify(newValue, null, 2);
+        $scope.output = computed(newValue.group);
+    }, true);
+}]);
+
+var queryBuilder = angular.module('queryBuilder', []);
+queryBuilder.directive('queryBuilder', ['$compile', function ($compile) {
+    return {
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        scope: {
+            group: '='
+        },
+        templateUrl: '/queryBuilderDirective.html',
+        compile: function (element, attrs) {
+            var content, directive;
+            content = element.contents().remove();
+            return function (scope, element, attrs) {
+                scope.operators = [
+                    { name: 'AND' },
+                    { name: 'OR' }
+                ];
+
+                scope.fields = [
+                    { name: 'Firstname' },
+                    { name: 'Lastname' },
+                    { name: 'Birthdate' },
+                    { name: 'City' },
+                    { name: 'Country' }
+                ];
+
+                scope.conditions = [
+                    { name: '=' },
+                    { name: '<>' },
+                    { name: '<' },
+                    { name: '<=' },
+                    { name: '>' },
+                    { name: '>=' }
+                ];
+
+                scope.addCondition = function () {
+                    scope.group.rules.push({
+                        condition: '=',
+                        field: 'Firstname',
+                        data: ''
+                    });
+                };
+
+                scope.removeCondition = function (index) {
+                    scope.group.rules.splice(index, 1);
+                };
+
+                scope.addGroup = function () {
+                    scope.group.rules.push({
+                        group: {
+                            operator: 'AND',
+                            rules: []
+                        }
+                    });
+                };
+
+                scope.removeGroup = function () {
+                    "group" in scope.$parent && scope.$parent.group.rules.splice(scope.$parent.$index, 1);
+                };
+
+                directive || (directive = $compile(content));
+
+                element.append(directive(scope, function ($compile) {
+                    return $compile;
+                }));
+            }
+        }
+    }
+}]);
---
+++ readme.md
@@ -0,0 +1,7 @@
+Angular Query Builder
+======================
+
+This is a sample HTML / JavaScript application that demonstrates how to use
+Angular.js to create a dynamic query building web UI.
+
+This was inspired by https://github.com/kindohm/knockout-query-builder
