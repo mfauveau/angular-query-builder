cd4354d450bd8145d37bb582c3eebdfe61822559
Matthieu Fauveau <mfauveau@gmail.com>
2014-07-18 00:09:34 -0400

    Fix output of <>, <= and >=

--- angular-query-builder.js
+++ angular-query-builder.js
@@ -2,13 +2,17 @@ var app = angular.module('app', ['ngSanitize', 'queryBuilder']);
 app.controller('QueryBuilderCtrl', ['$scope', function ($scope) {
     var data = '{"group": {"operator": "AND","rules": []}}';
 
+    function htmlEntities(str) {
+        return String(str).replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    }
+
     function computed(group) {
         if (!group) return "";
         for (var str = "(", i = 0; i < group.rules.length; i++) {
             i > 0 && (str += " <strong>" + group.operator + "</strong> ");
             str += group.rules[i].group ?
                 computed(group.rules[i].group) :
-                group.rules[i].field + " " + group.rules[i].condition + " " + group.rules[i].data;
+                group.rules[i].field + " " + htmlEntities(group.rules[i].condition) + " " + group.rules[i].data;
         }
 
         return str + ")";
